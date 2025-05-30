5b48b6eb18143a1bf2d609881f3479c24003739b
abdul rehman <ar03353505910@gmail.com>
2025-05-30 21:53:15 +0500

    update

---
+++ sdk-agent/translate.py
@@ -0,0 +1,51 @@
+#uv add openai-agents
+#uv add python-dotenv
+from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel, RunConfig
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+# Load the environment variables from .env file
+# Ensure you have a .env file with the GEMINI_API_KEY defined
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+print(gemini_api_key)
+
+
+# Check if the API key is present; if not, raise an error
+if not gemini_api_key:
+    raise ValueError("GEMINI_API_KEY is not set. Please ensure it is defined in your .env file.")
+
+#Reference: https://ai.google.dev/gemini-api/docs/openai
+external_client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.0-flash",
+    openai_client=external_client
+)
+
+config = RunConfig(
+    model=model,
+    model_provider=external_client,
+    tracing_disabled=True
+)
+
+# Write Agent
+writer = Agent(
+    name = 'Writer Agent',
+    instructions= 
+    """You are a translate agent. translate sentence into enlish to roman urdu  Generate poem,
+    stories, essay,etc."""
+)
+
+response = Runner.run_sync(
+    writer,
+    input = 'python notes on generative ai',
+    run_config = config
+)
+
+print(response.final_output)
